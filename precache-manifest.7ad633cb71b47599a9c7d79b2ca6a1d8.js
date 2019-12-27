83a61a101d51169f897673d92b402d8e30cad192
Jaydn <jaydn.1999@hotmail.com>
2019-12-27 12:58:57 +0000

    Updates

---
+++ precache-manifest.7ad633cb71b47599a9c7d79b2ca6a1d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d954493a186f09630f841f881e2c72b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34f0b6405ea70dc965a8",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3416e83b6e7778fb4818",
+    "url": "/static/js/2.a39d5d03.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a39d5d03.chunk.js.LICENSE"
+  },
+  {
+    "revision": "34f0b6405ea70dc965a8",
+    "url": "/static/js/main.ff230f38.chunk.js"
+  },
+  {
+    "revision": "d336f7b1ac5be40487fc",
+    "url": "/static/js/runtime-main.ddc60789.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
